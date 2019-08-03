51c2e17e27a6e85819e66e969f0c518b9b4a97f4
China_Zhou <kingship_zyz@163.com>
2019-08-03 15:53:25 +0800

    代码修改

--- src/main/java/com/xcxgf/cainiao/controller/BuildingController.java
+++ src/main/java/com/xcxgf/cainiao/controller/BuildingController.java
@@ -3,10 +3,9 @@ package com.xcxgf.cainiao.controller;
 import com.xcxgf.cainiao.POJO.Building;
 import com.xcxgf.cainiao.services.BuildingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -15,8 +14,29 @@ public class BuildingController {
     @Autowired
     private BuildingService bs;
 
-    @RequestMapping(method = RequestMethod.GET,value="/getBuildingList")
-    public List<Building> getBuildingList(){
+    @RequestMapping(method = RequestMethod.GET, value = "/getBuildingList")
+    public List<Building> getBuildingList() {
         return bs.getBuildingList();
+    }
+
+
+    @RequestMapping(method = RequestMethod.POST, value = "/updateBuildingList")
+    public List<Building> updateBuildingList(@RequestBody Building building) {
+        return bs.updateBuildingList(building);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/deleteBuildingList")
+    public List<Building> deleteBuildingList(@RequestBody Building building) {
+        return bs.deleteBuildingList(building);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/insertBuildingList")
+    public List<Building> insertBuildingList(@RequestBody Building building) {
+        return bs.insertBuildingList(building);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/insertBuildingListAll")
+    public List<Building> insertBuildingListAll(@RequestBody List<Building> buildingList) {
+        return bs.insertBuildingListAll(buildingList);
     }
 }
