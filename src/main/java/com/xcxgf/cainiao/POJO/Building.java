93cb95f9613bd60bffe04a378ee62abf58fb05d9
gddgong <1403515328@qq.com>
2019-08-05 13:04:37 +0800

    gdd

--- src/main/java/com/xcxgf/cainiao/POJO/Building.java
+++ src/main/java/com/xcxgf/cainiao/POJO/Building.java
@@ -4,11 +4,6 @@ public class Building {
     private int id;
     private String buildingNumber;
     private String buildingName;
-    private String roomArea;
-    private String hardwareSituation;
-    private String rent;
-    private String additionalConditions;
-    private int state;
 
     public int getId() {
         return id;
@@ -32,45 +27,5 @@ public class Building {
 
     public void setBuildingName(String buildingName) {
         this.buildingName = buildingName;
-    }
-
-    public String getRoomArea() {
-        return roomArea;
-    }
-
-    public void setRoomArea(String roomArea) {
-        this.roomArea = roomArea;
-    }
-
-    public String getHardwareSituation() {
-        return hardwareSituation;
-    }
-
-    public void setHardwareSituation(String hardwareSituation) {
-        this.hardwareSituation = hardwareSituation;
-    }
-
-    public String getRent() {
-        return rent;
-    }
-
-    public void setRent(String rent) {
-        this.rent = rent;
-    }
-
-    public String getAdditionalConditions() {
-        return additionalConditions;
-    }
-
-    public void setAdditionalConditions(String additionalConditions) {
-        this.additionalConditions = additionalConditions;
-    }
-
-    public int getState() {
-        return state;
-    }
-
-    public void setState(int state) {
-        this.state = state;
     }
 }
