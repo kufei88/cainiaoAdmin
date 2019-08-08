00aa0315a0d38ab0dcb332b8007d6c02efeddfd3
Administrator <1931253744@qq.com>
2019-08-08 20:25:39 +0800

    田易修改

--- src/main/java/com/xcxgf/cainiao/controller/BuildingController.java
+++ src/main/java/com/xcxgf/cainiao/controller/BuildingController.java
@@ -6,8 +6,6 @@ import com.xcxgf.cainiao.POJO.Building;
 import com.xcxgf.cainiao.services.BuildingService;
 import org.springframework.beans.factory.annotation.Autowired;
 
-<<<<<<< HEAD
-=======
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
->>>>>>> 9f07f288bd3fe2e3312284a8dfc4e6dbd4c3250c
 import java.util.List;
 
 @RestController
--- src/main/java/com/xcxgf/cainiao/mapper/BuildingMapper.java
+++ src/main/java/com/xcxgf/cainiao/mapper/BuildingMapper.java
@@ -8,16 +8,4 @@ import java.util.List;
 public interface BuildingMapper {
     @Select("select * from buildingInfo")
     public List<Building> getBuildingList();
-<<<<<<< HEAD
-
-    @Update("UPDATE buildingInfo SET buildingNumber=#{buildingNumber},buildingName=#{buildingName} WHERE id =#{id}")
-    public void updateBuildingInfo(Building building);
-
-    @Update("UPDATE buildingInfo SET state=-1 WHERE id =#{id}")
-    public void deleteBuildingInfo(Building building);
-
-    @Insert("INSERT INTO buildingInfo(buildingNumber,buildingName) VALUES(#{buildingNumber}, #{buildingName})")
-    public void insertBuildingInfo(Building building);
-=======
->>>>>>> 9f07f288bd3fe2e3312284a8dfc4e6dbd4c3250c
 }
