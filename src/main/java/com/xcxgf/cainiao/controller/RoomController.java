6fbc3bf99e9eca3e71831dad0bacac59ccb71dff
China_Zhou <kingship_zyz@163.com>
2019-08-08 22:43:02 +0800

    租赁模块后端v1.0.1

---
+++ src/main/java/com/xcxgf/cainiao/controller/RoomController.java
@@ -0,0 +1,41 @@
+package com.xcxgf.cainiao.controller;
+
+import com.xcxgf.cainiao.POJO.Room;
+import com.xcxgf.cainiao.services.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("room")
+public class RoomController {
+    @Autowired
+    private RoomService rs;
+
+    @RequestMapping(method = RequestMethod.GET,value = "/getRoomList")
+    public List<Room> getRoomList(){
+        return rs.getRoomList();
+    }
+
+    @RequestMapping(method = RequestMethod.POST,value = "/deleteRoomList")
+    public List<Room> deleteRoomList(@RequestBody Room room){
+        rs.deleteRoomList(room);
+        return rs.getRoomList();
+    }
+
+    @RequestMapping(method = RequestMethod.POST,value = "/insertRoomList")
+    public List<Room> insertRoomList(@RequestBody Room room){
+        rs.insertRoomList(room);
+        return rs.getRoomList();
+    }
+
+    @RequestMapping(method = RequestMethod.POST,value = "/updateRoomList")
+    public List<Room> updateRoomList(@RequestBody Room room){
+        rs.updateRoomList(room);
+        return rs.getRoomList();
+    }
+}
--- src/main/java/com/xcxgf/cainiao/services/BuildingService.java
+++ src/main/java/com/xcxgf/cainiao/services/BuildingService.java
@@ -16,4 +16,29 @@ public class BuildingService {
     public List<Building> getBuildingList(){
         return buildingMapper.getBuildingList();
     }
+<<<<<<< HEAD
+
+    public List<Building> updateBuildingList(Building building){
+        buildingMapper.updateBuildingInfo(building);
+        return buildingMapper.getBuildingList();
+    }
+
+    public List<Building> deleteBuildingList(Building building){
+        buildingMapper.deleteBuildingInfo(building);
+        return buildingMapper.getBuildingList();
+    }
+
+    public List<Building> insertBuildingList(Building building){
+        buildingMapper.insertBuildingInfo(building);
+        return buildingMapper.getBuildingList();
+    }
+
+    public List<Building> insertBuildingListAll(List<Building> buildingList){
+        for (Building building: buildingList) {
+            buildingMapper.insertBuildingInfo(building);
+        }
+        return buildingMapper.getBuildingList();
+    }
+=======
+>>>>>>> 9f07f288bd3fe2e3312284a8dfc4e6dbd4c3250c
 }
