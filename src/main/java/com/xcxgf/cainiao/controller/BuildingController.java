518ea217125c8c2f6201bc034506c3a5d6f58b1e
Administrator <1931253744@qq.com>
2019-08-09 19:38:05 +0800

    田易修改5

--- src/main/java/com/xcxgf/cainiao/controller/BuildingController.java
+++ src/main/java/com/xcxgf/cainiao/controller/BuildingController.java
@@ -6,10 +6,13 @@ import com.xcxgf.cainiao.POJO.Building;
 import com.xcxgf.cainiao.services.BuildingService;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 import org.springframework.web.bind.annotation.RequestMethod;
+
+
 import java.util.List;
 
 @RestController
@@ -21,24 +24,6 @@ public class BuildingController {
 
     @RequestMapping(method = RequestMethod.GET,value="/getBuildingList")
     public List<Building> getBuildingList(){
-        return bs.getBuildingList();
-    }
-
-    @RequestMapping(method = RequestMethod.POST,value="/deleteBuildingList")
-    public List<Building> deleteBuildingList(@RequestBody Building building){
-        bs.deleteBuildingList(building);
-        return bs.getBuildingList();
-    }
-
-    @RequestMapping(method = RequestMethod.POST,value="/updateBuildingList")
-    public List<Building> updateBuildingList(@RequestBody Building building){
-        bs.updateBuildingList(building);
-        return bs.getBuildingList();
-    }
-
-    @RequestMapping(method = RequestMethod.POST,value="/insertBuildingList")
-    public List<Building> insertBuildingList(@RequestBody Building building){
-        bs.insertBuildingList(building);
         return bs.getBuildingList();
     }
 
