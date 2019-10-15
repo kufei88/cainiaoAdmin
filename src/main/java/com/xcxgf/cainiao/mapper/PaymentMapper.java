a5a37673321e6abdb2827c77eadafca07f0021a3
China_Zhou <kingship_zyz@163.com>
2019-10-15 09:19:46 +0800

    zyz2019-10-15

--- src/main/java/com/xcxgf/cainiao/mapper/PaymentMapper.java
+++ src/main/java/com/xcxgf/cainiao/mapper/PaymentMapper.java
@@ -70,7 +70,7 @@ public interface PaymentMapper {
      * @param owner
      * @return
      */
-    @Select("select  count(*) from paymentinfo where owner LIKE CONCAT('%',#{owner},'%')")
+    @Select("select  count(*) from paymentinfo where owner = #{owner}")
     public int getSearchOwnerCount(String owner);
 
     /**
